c86205d31c0783cd41b05284050805c569492786
karl-s-fe <karl-s-fe@gate.me>
2024-10-23 15:48:33 +0800

    feat: init

--- remix.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('@remix-run/dev').AppConfig} */
-module.exports = {
-  serverBuildTarget: "netlify",
-  server: "./server.js", // Netlify 需要一个自定义服务器
-  ignoredRouteFiles: ["**/.*"],
-};
--- server.js
+++
@@ -1,6 +0,0 @@
-import { createRequestHandler } from "@remix-run/netlify";
-
-export const handler = createRequestHandler({
-  // Enable this if you want to forcefully purge the cache of your app
-  // cache: 'manual'
-});
