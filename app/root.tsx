2a0c8a49b8515de1554f8dee36c9c7c6ce80b62f
karl-s-fe <karl-s-fe@gate.me>
2024-10-29 15:39:13 +0800

    feat: add google analycis

--- app/root.tsx
+++ app/root.tsx
@@ -1,8 +1,20 @@
-import { Links, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react';
+import { useEffect } from 'react';
+import { json } from '@remix-run/node';
+import {
+	Links,
+	Meta,
+	Outlet,
+	Scripts,
+	ScrollRestoration,
+	useLocation,
+	useLoaderData,
+} from '@remix-run/react';
 import type { LinksFunction, LoaderFunctionArgs, SerializeFrom } from '@remix-run/node';
 import Header from '~/components/Header';
 import Footer from '~/components/Footer';
 import { getDomainUrl } from './utils/misc';
+
+import * as gtag from '~/utils/gtags.client';
 
 import '~/styles/tailwind.css';
 
@@ -11,11 +23,23 @@ export const links: LinksFunction = () => [];
 export async function loader({ request }: LoaderFunctionArgs) {
 	return {
 		requestInfo: { origin: getDomainUrl(request), path: new URL(request.url).pathname },
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		gaTrackingId: json({ gaTrackingId: process.env.GA_TRACKING_ID }) as any,
 	};
 }
 export type RootLoaderType = SerializeFrom<typeof loader>;
 
 function App() {
+	const location = useLocation();
+
+	const { gaTrackingId } = useLoaderData<typeof loader>();
+
+	useEffect(() => {
+		if (gaTrackingId?.length) {
+			gtag.pageview(location.pathname, gaTrackingId);
+		}
+	}, [location, gaTrackingId]);
+
 	return (
 		<html lang="en">
 			<head>
@@ -25,6 +49,26 @@ function App() {
 				<Links />
 			</head>
 			<body>
+				{process.env.NODE_ENV === 'development' || !gaTrackingId ? null : (
+					<>
+						<script async src={`https://www.googletagmanager.com/gtag/js?id=${gaTrackingId}`} />
+						<script
+							async
+							id="gtag-init"
+							dangerouslySetInnerHTML={{
+								__html: `
+									window.dataLayer = window.dataLayer || [];
+									function gtag(){dataLayer.push(arguments);}
+									gtag('js', new Date());
+
+									gtag('config', '${gaTrackingId}', {
+									page_path: window.location.pathname,
+									});
+								`,
+							}}
+						/>
+					</>
+				)}
 				<div className="flex min-h-screen flex-col">
 					<Header />
 					<main className="relative mx-auto my-0 box-border flex w-full max-w-7xl flex-[1] flex-grow flex-col py-[1em] px-[2em]">
